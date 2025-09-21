5cd8f424deac6ce893b3a54ef8bf0659d3a42dea
Sherwan <sherwanuodcs@gmail.com>
2025-09-21 21:29:04 +0200

    allow frontend url to call backend api

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,6 +4,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix("api");
+    app.enableCors({
+    origin: 'http://localhost:5173', // Vuejs url
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
